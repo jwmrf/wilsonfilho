b35fe948c20299d90f3ca26d98ab8ead118a05b8
josé wilson <wilsonfilho.dev@gmail.com>
2024-10-05 20:47:54 -0300

    path resolve

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,10 @@
+const path = require("path");
+
 module.exports = {
   i18n: {
     defaultLocale: 'pt',
     locales: ['pt', 'en'],
   },
   defaultNS: 'common',
-  localePath: './public/locales',
+  localePath: path.resolve('./public/locales'),
 };
