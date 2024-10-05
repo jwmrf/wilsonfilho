dd49126762828638d6522be16aa62ea0d53c3cb2
josé wilson <wilsonfilho.dev@gmail.com>
2024-10-05 04:07:44 -0300

    Ajuste 2

--- next.config.js
+++ next.config.js
@@ -3,10 +3,7 @@ const { i18n } = require('./next-i18next.config');
 
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['pt', 'en'],
-    defaultLocale: 'pt',
-  },
+  i18n,
   images: {
     domains: ['avatars.githubusercontent.com'],
   },
